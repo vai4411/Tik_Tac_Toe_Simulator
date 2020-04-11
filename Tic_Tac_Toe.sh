b3e046f95b39111ff555a148a1825911ba47730f
root <root@localhost.localdomain>
2020-04-11 19:39:13 +0000

    ADDED: Player Start With Fresh Board

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,2 +1,30 @@
 #!/bin/bash -x
 echo "Welcome to Tic Tac Toe Game"
+
+#variable
+position=0
+one=1
+two=2
+three=3
+four=4
+five=5
+six=6
+seven=7
+eight=8
+nine=9
+
+declare -a board
+
+for (( position=1 ; position<=9 ; position++ ))
+do
+	board[$position]=$position
+done
+
+function printBoard() {
+	echo " ${board[$one]} | ${board[$two]} | ${board[$three]}"
+	echo "-----------"
+	echo " ${board[$four]} | ${board[$five]} | ${board[$six]}"
+   echo "-----------"
+	echo " ${board[$seven]} | ${board[$eight]} | ${board[$nine]}"
+}
+printBoard
